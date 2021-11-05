0098b18645cf846ce1f9ad4953433fae3a50aa37
Kruaks17 <aksel.kj@hotmail.com>
2021-11-05 11:12:07 +0100

    commit

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,114 +1,130 @@
 import React, { useEffect, useState } from "react";
-import {  useMotionValue, useSpring } from "framer-motion";
+import { useMotionValue, useSpring } from "framer-motion";
 
-import sanityClient  from "../client.js";
+// import sanityClient  from "../client.js";
 
-export default function Project(){
-    const [ projectData, setProjectData ] = useState(null);
+export default function Project() {
+    const [projectData, setProjectData] = useState(null);
 
     const [cursorXY, setCursorXY] = useState({ x: -100, y: -100 });
 
-     const cursorX = useMotionValue(-100);
-     const cursorY = useMotionValue(-100);
+    const cursorX = useMotionValue(-100);
+    const cursorY = useMotionValue(-100);
 
-     const springConfig = { damping: 30, stiffness: 800 };
+    const springConfig = { damping: 30, stiffness: 800 };
     //  const cursorXSpring = useSpring(cursorX, springConfig);
     //  const cursorYSpring = useSpring(cursorY, springConfig);
 
-     useEffect(() => {
+    useEffect(() => {
 
-         const moveCursor = (e) => {
+        const moveCursor = (e) => {
 
-             cursorX.set(e.clientX - 16)
+            cursorX.set(e.clientX - 16)
             cursorY.set(e.clientY - 16)
 
-             const x = e.clientX - 16
+            const x = e.clientX - 16
             const y = e.clientY - 16
-             setCursorXY({ x, y })
+            setCursorXY({ x, y })
 
-         }
+        }
 
-         window.addEventListener('mousemove', moveCursor);
+        window.addEventListener('mousemove', moveCursor);
 
 
         return () => {
-             window.removeEventListener('mousemove', moveCursor);
+            window.removeEventListener('mousemove', moveCursor);
 
-         }
+        }
 
-     }, []);
+    }, []);
 
-    useEffect (() =>{
+    // useEffect (() =>{
 
-        sanityClient.fetch(`*[_type == "project"]{
-            Image,
-            title,
-            date,
-            place,
-            description,
-            projectType,
-            link,
-            tags,
-            
-        }`).then((data)=> setProjectData(data))
-           .catch((console.error));
-    },[]);
+    //     sanityClient.fetch(`*[_type == "project"]{
+    //         Image,
+    //         title,
+    //         date,
+    //         place,
+    //         description,
+    //         projectType,
+    //         link,
+    //         tags,
 
-    return(
+    //     }`).then((data)=> setProjectData(data))
+    //        .catch((console.error));
+    // },[]);
+
+    return (
         <>
-        <main  style={{color:"#EBF5DF"}} >
-        <div className="cursor"
+            <main style={{ color: "#EBF5DF" }} >
+                <div className="cursor"
                     style={{
                         transform: `translate3d(${cursorXY.x}px, ${cursorXY.y}px, 0) `,
                         translateX: cursorX, translateY: cursorY,
                     }}
-                /> 
-            <section >
-                <h1></h1>
-                <h2></h2>
-                <section className="container-post">
-                    {projectData && projectData.map((project, index)=> (
-                    <article >
-                        <h1>
-                            <a 
-                                tabIndex={0}
-                                href={project.link}
-                                alt={project.title}
-                                rel="noreferrer"
-                                target="_blank">
-                                {project.title}
-                            </a>
-                        </h1>
-                        
-                        
-                        <div >
-                            <span>
-                                {project.ProjectType}
-                            </span>
-                            <p>
-                                {project.description}
-                            </p>
-                            <a tabIndex={0} href={project.link} target={"_blank"} rel="noreferrer">
-                                View the project{""}
-                            <span role="img" arialabel="right pointer">
-                            </span>
-                            </a>
-                            <span style={{fontFamily:"ObjectSans-Thin"}}>
-                                {project.tags}
-                            </span>
-                        </div>
-                    </article>
-                    ))}
+                />
+                <section >
+                    <section className="container-post">
+                    
+                    <div>
+                    <img className="prosjekt-bilde"
+                             alt="prosjekt-bilde"
+                             style={{ width: '80%' }}
+                             src={process.env.PUBLIC_URL + '../BB.png'} />
+                    <h1>Børres Burger</h1>
+                    <p style={{fontFamily:"ObjectSans-Thin", fontSize:"1rem"}}>Nettside</p>    
+                    </div>
+                    <div>
+                    <img className="prosjekt-bilde"
+                             alt="prosjekt-bilde"
+                             style={{ width: '80%' }}
+                             src={process.env.PUBLIC_URL + '../Cimera_.png'} />
+                    <h1>Cimera_</h1>
+                    <p style={{fontFamily:"ObjectSans-Thin", fontSize:"1rem"}}>Nettside</p>
+                    </div>
+                    <div>
+                    <img className="prosjekt-bilde"
+                             alt="prosjekt-bilde"
+                             style={{ width: '80%' }}
+                             src={process.env.PUBLIC_URL + '../PP.png'} />
+                    <h1>Paragraph Paragraph</h1>
+                    <p style={{fontFamily:"ObjectSans-Thin", fontSize:"1rem"}}>Nettside</p>
+                    </div>
+                    <div>
+                    <img className="prosjekt-bilde"
+                             alt="prosjekt-bilde"
+                             style={{ width: '80%' }}
+                             src={process.env.PUBLIC_URL + '../Artboard 1.jpg'} />
+                    <h1>North Men</h1>
+                    <p style={{fontFamily:"ObjectSans-Thin", fontSize:"1rem"}}>Pakkningdesign</p>
+                    </div>
+                    <div>
+                    <img className="prosjekt-bilde"
+                             alt="prosjekt-bilde"
+                             style={{ width: '80%' }}
+                             src={process.env.PUBLIC_URL + '../Photolouge.jpg'} />
+                    <h1>Photolouge.</h1>
+                    <p style={{fontFamily:"ObjectSans-Thin", fontSize:"1rem"}}>Redaksjonellt design</p>
+                    </div>
+                    <div>
+                    <img className="prosjekt-bilde"
+                             alt="prosjekt-bilde"
+                             style={{ width: '80%' }}
+                             src={process.env.PUBLIC_URL + '../Kinetic-Animation_v2.gif'} />
+                    <h1>Animasjon</h1>
+                    <p style={{fontFamily:"ObjectSans-Thin", fontSize:"1rem"}}>After Effects</p>
+                    </div>
+
+                    </section>
                 </section>
-            </section>
-            <footer> 
-         
-        <a href="https://www.instagram.com/akselkj/" target="_blank" rel="noreferrer" alt="link til instagram"><p>Instagram</p></a>
-        <a href="https://www.linkedin.com/in/aksel-kruse-jensen-4a862a1a1/" target="_blank" rel="noreferrer" alt="link til linkedin"> <p>LinkedIn</p></a>
-        <a href="https://github.com/Kruaks17" target="_blank" rel="noreferrer" alt="link til github"><p>Github</p></a>
-        </footer>
-        </main>
-        
+
+            </main>
+            <footer>
+                <a href="https://www.instagram.com/akselkj/" target="_blank" rel="noreferrer" alt="link til instagram"><p>Instagram</p></a>
+                <a href="https://www.linkedin.com/in/aksel-kruse-jensen-4a862a1a1/" target="_blank" rel="noreferrer" alt="link til linkedin"> <p>LinkedIn</p></a>
+                <a href="https://github.com/Kruaks17" target="_blank" rel="noreferrer" alt="link til github"><p>Github</p></a>
+            </footer>
+
         </>
     )
 
